1a3d39caf99eb9803356ed3c8eb285b4973457e0
dowwo <roggers.morales@gmail.com>
2021-05-22 01:54:15 -0400

    Update Test.php

--- app/Controllers/Test.php
+++ app/Controllers/Test.php
@@ -6,6 +6,6 @@ class Test extends BaseController
 {
 	public function index()
 	{
-		echo "<h1>Hola</h1>";
+		echo "<h1>Hola</h1></br><h3>Si estás leyendo esto entonces te diste cuenta que estaba probando un controlador</h3></br><h4>Como se ve aquí, es necesario solo el controlador para crear una vista, pero lo que debemos ahcer es mostrar desde el controlador la vista</h4>";
 	}
 }
