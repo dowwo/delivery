06a175c0c9641f91d508957adce20f183607f8ce
dowwo <roggers.morales@gmail.com>
2021-06-26 14:38:34 -0400

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -32,7 +32,7 @@ class Database extends Config
 	 */
 	public $default = [
 		'DSN'      => '',
-		'hostname' => 'localhost',
+		'hostname' => 'valefor.delivery-chile.cl',
 		'username' => 'deliver2_dcla',
 		'password' => '/-791348265-/',
 		'database' => 'deliver2_bddeliverycl',
