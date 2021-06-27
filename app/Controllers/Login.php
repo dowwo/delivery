9a9841e99e8ee9e5832e86f335277c9abf53b698
dowwo <roggers.morales@gmail.com>
2021-06-26 22:25:11 -0400

    Update Login.php

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -29,7 +29,7 @@ class Login extends Controller
                     'logged_in'     => TRUE
                 ];
                 $session->set($ses_data);
-                return redirect()->to('dashboard');
+                return redirect()->to(base_url('dashboard');
             }else{
                 $session->setFlashdata('msg', 'Wrong Password');
                 return redirect()->to('/login');
