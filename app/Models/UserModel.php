5c0aa3b840881459e03e937c623df015fee6f8f5
dowwo <roggers.morales@gmail.com>
2021-07-03 20:33:22 -0400

    Update UserModel.php

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,10 +8,10 @@ use CodeIgniter\Model;
 
 class UserModel extends Model
 {
-    /* Lo comento por si hay errores, luego deberiamos quitar estas lineas comentadas
-    protected $table = 'user';
-    protected $allowedFields = ['user_name','user_email','user_password','user_created_at'];
-    */
+    // Lo comento por si hay errores, luego deberiamos quitar estas lineas comentadas
+    //protected $table = 'user';
+    //protected $allowedFields = ['user_name','user_email','user_password','user_created_at'];
+    //
     //Con la base de datos real, debería ir así
     protected $table = 'usuario';
     protected $allowedFields = ['nombre','apellido_p','apellido_m','email','password'];
