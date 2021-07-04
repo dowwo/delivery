bf2f09274452d7c2f419b461c1f25868997e669a
dowwo <roggers.morales@gmail.com>
2021-07-03 20:19:14 -0400

    Update Login.php

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -17,25 +17,30 @@ class Login extends Controller
         $model = new UserModel();
         $email = $this->request->getVar('email');
         $password = $this->request->getVar('password');
-        $data = $model->where('user_email', $email)->first();
+        //$data = $model->where('user_email', $email)->first();
+        $data = $model->where('email', $email)->first();
         if($data){
-            $pass = $data['user_password'];
+            //$pass = $data['user_password'];
+            $pass = $data['password'];
             $verify_pass = password_verify($password, $pass);
             if($verify_pass){
                 $ses_data = [
-                    'user_id'       => $data['user_id'],
-                    'user_name'     => $data['user_name'],
-                    'user_email'    => $data['user_email'],
+                    //'user_id'       => $data['user_id'],
+                    //'user_name'     => $data['user_name'],
+                    //'user_email'    => $data['user_email'],
+                    'id_usuario'       => $data['id_usaurio'],
+                    'nombre'     => $data['nombre'],
+                    'email'    => $data['email'],
                     'logged_in'     => TRUE
                 ];
                 $session->set($ses_data);
-                return redirect()->to('/dashboard');
+                return redirect()->to('/dashboard1');
             }else{
-                $session->setFlashdata('msg', 'Wrong Password');
+                $session->setFlashdata('msg', 'Contraseña incorrecta');
                 return redirect()->to('/login');
             }
         }else{
-            $session->setFlashdata('msg', 'Email not Found');
+            $session->setFlashdata('msg', 'Email no encontrado');
             return redirect()->to('/login');
         }
     }
