7c7645a7c230d779ef35755915d8a526ba710619
dowwo <roggers.morales@gmail.com>
2021-07-14 01:44:25 -0400

    Update Routes.php

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -44,7 +44,7 @@ $routes->get('/dashboard3', 'DashboardAdmin::index',['filter' => 'admin']);
 
 //Ruta que da acceso a la página de registro de usuario
 $routes->get('/registro', 'Register::index',['filter' => 'auth']);
-
+$routes->get('/register','Register::index',['filter' => 'auth']);
 $routes->get('/lista_usuarios','UsuarioController::index',['filter' => 'admin']);
 
 //
