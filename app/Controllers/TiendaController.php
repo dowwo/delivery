0810cea2e94c09bed4862d451552d8fccf8b0c0c
dowwo <roggers.morales@gmail.com>
2021-07-15 18:59:52 -0400

    2

--- app/Controllers/TiendaController.php
+++ app/Controllers/TiendaController.php
@@ -4,7 +4,17 @@
 namespace App\Controllers;
 
 
-class TiendaController
-{
+use CodeIgniter\Controller;
+use CodeIgniter\Model;
 
+class TiendaController extends Controller
+{
+    // show users list
+    public function index(){
+        $modeloTienda = new TiendaModel();
+        $data['tienda'] = $modeloTienda->orderBy('id_tienda', 'DESC')->findAll();
+        // Para las vistas que se encuentran en subvcarpetas se realiza de la siguiente manera
+        // return view('carpeta/vista', $data);
+        return view('lista_usuarios', $data);
+    }
 }
