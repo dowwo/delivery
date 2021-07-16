764f59467f25e76c58d6a9d4ef7ffed3ec596a5b
dowwo <roggers.morales@gmail.com>
2021-07-16 00:59:22 -0400

    Update UsuarioController.php

--- app/Controllers/UsuarioController.php
+++ app/Controllers/UsuarioController.php
@@ -38,8 +38,8 @@ class UsuarioController extends Controller
     // delete user
     public function delete($id = null){
         $modeloUsuario = new UserModel();
-        $data['usuarios'] = $modeloUsuario->where('id_usuario', $id);
-        $modeloUsuario->delete($id);
+        $id_usuario['usuarios'] = $modeloUsuario->where('id_usuario', $id);
+        $modeloUsuario->delete($id_usuario);
 
         return $this->response->redirect(site_url('/lista_usuarios'));
     }
