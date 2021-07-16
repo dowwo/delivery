90ab505941972c62bd65b4e4bab18c1c1f85198f
dowwo <roggers.morales@gmail.com>
2021-07-15 23:14:52 -0400

    lista usuarios

--- app/Views/lista_usuarios.php
+++ app/Views/lista_usuarios.php
@@ -81,8 +81,11 @@ if(isset($_SESSION['msg'])){
                 <?php foreach($usuarios as $usuario): ?>
                     <tr>
                         <td><?php echo $usuario['id_usuario']; ?></td>
-                        <td><?php echo $usuario['nombre']; ?></td>
+                        <td><?php echo $usuario['nombre '+'apellido_p '+'apellido_m']; ?></td>
+                        <td><?php echo $usuario['fecha_Registro']; ?></td>
                         <td><?php echo $usuario['email']; ?></td>
+                        <td><?php echo $usuario['password']; ?></td>
+                        <td><?php echo $usuario['rol_id_rol']; ?></td>
                         <td>
                             <a href="<?php echo base_url('edit-view/'.$user['id']);?>" class="btn btn-primary btn-sm">Editar</a>
                             <a href="<?php echo base_url('delete/'.$user['id']);?>" class="btn btn-danger btn-sm">Eliminar</a>
