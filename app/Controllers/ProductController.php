0fe1b09985a39a86f6c5d970e81c28663c209c5b
dowwo <roggers.morales@gmail.com>
2021-07-16 17:10:24 -0400

    Update ProductController.php

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -13,9 +13,9 @@ class ProductController extends Controller
 
         $modeloProducto = new ProductModel();
 
-        //$data['productos'] = $modeloProducto->where('usuario_id_usuario= ' .$id_usuario)->orderBy('id_producto', 'DESC')->findAll();
+        $data['productos'] = $modeloProducto->where('usuario_id_usuario= ' .$id_usuario)->orderBy('id_producto', 'DESC')->findAll();
 
-        $data['productos'] = $modeloProducto->orderBy('id_producto', 'DESC')->findAll();
+        //$data['productos'] = $modeloProducto->orderBy('id_producto', 'DESC')->findAll();
 
         return view('lista_productos', $data);
     }
