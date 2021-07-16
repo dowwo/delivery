20aa3c12e413ba5fd42eba64a48a214aca9a41ab
dowwo <roggers.morales@gmail.com>
2021-07-16 14:09:14 -0400

    Update TiendaController.php

--- app/Controllers/TiendaController.php
+++ app/Controllers/TiendaController.php
@@ -1,1 +1,33 @@
-asdasd
+<?php
+
+
+namespace App\Controllers;
+
+
+use App\Models\TiendaModel;
+use CodeIgniter\Controller;
+use CodeIgniter\Model;
+
+class TiendaController extends Controller
+{
+    // show tienda list
+    public function index(){
+
+        $modeloTienda = new TiendaModel();
+
+        $data['tiendas'] = $modeloTienda->getWhere(['usuario_id_usuario' => $_SESSION['id_usuario']]);
+
+
+
+        // Para las vistas que se encuentran en subcarpetas se realiza de la siguiente manera
+        // return view('carpeta/vista', $data);
+        return view('lista_tienda', $data);
+    }
+
+    //Show single tienda
+    public function singleTienda($id = null){
+        $modeloTienda = new TiendaModel();
+        $data['tienda_obj'] = $modeloTienda->where('id_tienda', $id)->first();
+        return view('modificar_tienda', $data);
+    }
+}
