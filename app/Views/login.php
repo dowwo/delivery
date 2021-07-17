3e1c328adca2d96392f1359d250f6b8936942902
dowwo <roggers.morales@gmail.com>
2021-07-16 23:46:09 -0400

    Update login.php

--- app/Views/login.php
+++ app/Views/login.php
@@ -53,37 +53,25 @@
 </head>
 <body>
 
-<nav class="navbar navbar-default navbar-fixed-top">
-    <div class="container">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand" href="#myPage">Logo</a>
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-            <ul class="nav navbar-nav navbar-right">
-                <li><a href="../">DELIVERY CHILE</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
+
 <div>
-    <div class="container">
-        <div class="row">
-            <div class="col-sm">
-                One of three columns
+    <nav class="navbar navbar-default navbar-fixed-top">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#myPage">Logo</a>
             </div>
-            <div class="col-sm">
-                One of three columns
-            </div>
-            <div class="col-sm">
-                One of three columns
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="../">DELIVERY CHILE</a></li>
+                </ul>
             </div>
         </div>
-    </div>
+    </nav>
 
 </div>
 
