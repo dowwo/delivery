2dc17d93e0068f2e8ba40a005abcc68f51c0324e
dowwo <roggers.morales@gmail.com>
2021-07-16 23:32:18 -0400

    Update login.php

--- app/Views/login.php
+++ app/Views/login.php
@@ -26,6 +26,31 @@
     <title>Login</title>
 </head>
 <body>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#myPage">Logo</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#quienes_somos">¿QUIENES SOMOS?</a></li>
+                <li><a href="#nuestros_valores">NUESTROS VALORES</a></li>
+                <!--
+                <li><a href="#portfolio">PORTFOLIO</a></li>
+                <li><a href="#pricing">PRICING</a></li>
+                -->
+                <li><a href="#contacto">CONTACTO</a></li>
+                <li><a href="login" class="btn btn-success">Iniciar sesión</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
 <div>
 
 </div>
