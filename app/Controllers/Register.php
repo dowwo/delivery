4f707b650c34d9269ec66b544780e91e0ebbd81d
dowwo <roggers.morales@gmail.com>
2021-07-17 13:31:47 -0400

    Update Register.php

--- app/Controllers/Register.php
+++ app/Controllers/Register.php
@@ -19,9 +19,9 @@ class Register extends Controller
         helper(['form']);
         //set rules validation form
         $rules = [
-            'nombre'          => 'required|min_length[3]|max_length[20]',
-            'apellido_p'          => 'required|min_length[3]|max_length[20]',
-            'apellido_m'          => 'required|min_length[3]|max_length[20]',
+            'nombre'        => 'required|min_length[3]|max_length[20]',
+            'apellido_p'    => 'required|min_length[3]|max_length[20]',
+            'apellido_m'    => 'required|min_length[3]|max_length[20]',
             'email'         => 'required|min_length[6]|max_length[50]|valid_email|is_unique[usuario.email]',
             'password'      => 'required|min_length[6]|max_length[200]',
             'confpassword'  => 'matches[password]'
@@ -36,8 +36,8 @@ class Register extends Controller
                 'apellido_m'        => $this->request->getVar('apellido_m'),
                 'fecha_registro'    =>  $this->request->getVar('fecha_registro'),
                 'email'             => $this->request->getVar('email'),
-                'password'          => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
-                'rol_id_rol'        => $this->request->getVar('rol_id_rol')
+                'password'          => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT)
+                //'rol_id_rol'        => $this->request->getVar('rol_id_rol')
             ];
             $model->save($data);
             return redirect()->to('/login');
