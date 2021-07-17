932c63ffe9be0c8eacf22062c77eff645e48e47f
dowwo <roggers.morales@gmail.com>
2021-07-16 21:07:38 -0400

    Update login.php

--- app/Views/login.php
+++ app/Views/login.php
@@ -8,7 +8,8 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
     <style>
         body {
-            background-color: rgba(0, 50, 100, 0.5);
+            background-color: #AB3E5B;
+            <!--background-color: rgba(0, 50, 100, 0.5);-->
             padding-top: 5%;
         }
         #formulario {
