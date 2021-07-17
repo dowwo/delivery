e2efa7d177e51c04df6630717296d510f13040b9
Cristofer-Sepulveda <cristofer.sepulveda02@gmail.com>
2021-07-17 06:52:10 -0400

    cambios minimos

--- app/Views/login.php
+++ app/Views/login.php
@@ -10,9 +10,6 @@
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-
-
-
     <style>
         body {
             background-color: #AB3E5B;
@@ -46,71 +43,58 @@
             border-color: transparent;
             color: #fff !important;
         }
-
     </style>
-
-    <title>Login</title>
+    <title>Iniciar sesión</title>
 </head>
 <body>
-
-
-<div>
-    <nav class="navbar navbar-default navbar-fixed-top">
+    <div>
+        <nav class="navbar navbar-default navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#myPage">Logo</a>
+                </div>
+                <div class="collapse navbar-collapse" id="myNavbar">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="../">DELIVERY CHILE</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+    <div>
         <div class="container">
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="#myPage">Logo</a>
+            <div class="row justify-content-md-center">
+                <div class="col-md-3"></div>
+                <div class="col-md-6" id="formulario">
+                    <h1>Iniciar sesión</h1>
+                    <?php if(session()->getFlashdata('msg')):?>
+                        <div class="alert alert-danger"><?= session()->getFlashdata('msg') ?></div>
+                    <?php endif;?>
+                    <form action="login/auth" method="post">
+                        <div class="mb-3">
+                            <label for="InputEmail" class="form-label">Email</label>
+                            <input type="email" name="email" class="form-control" id="InputEmail" value="<?= set_value('email') ?>">
+                        </div>
+                        <div class="mb-3">
+                            <label for="InputPassword" class="form-label">Contraseña</label>
+                            <input type="password" name="password" class="form-control" id="InputPassword">
+                        </div>
+                        <br>
+                        <div>
+                            <button type="submit" class="btn btn-primary">Iniciar sesión</button>
+                        </div>
+                        <div></div>
+                        <br>
+                    </form>
+                </div>
             </div>
-            <div class="collapse navbar-collapse" id="myNavbar">
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="../">DELIVERY CHILE</a></li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
-</div>
-<div>
-    <div class="container">
-        <div class="row justify-content-md-center">
-
-            <div class="col-md-3"></div>
-            <div class="col-md-6" id="formulario">
-                <h1>Iniciar sesión</h1>
-                <?php if(session()->getFlashdata('msg')):?>
-                    <div class="alert alert-danger"><?= session()->getFlashdata('msg') ?></div>
-                <?php endif;?>
-                <form action="login/auth" method="post">
-                    <div class="mb-3">
-                        <label for="InputEmail" class="form-label">Email</label>
-                        <input type="email" name="email" class="form-control" id="InputEmail" value="<?= set_value('email') ?>">
-                    </div>
-                    <div class="mb-3">
-                        <label for="InputPassword" class="form-label">Contraseña</label>
-                        <input type="password" name="password" class="form-control" id="InputPassword">
-                    </div>
-                    <br>
-                    <div>
-                        <button type="submit" class="btn btn-primary">Iniciar sesión</button>
-                    </div>
-                    <div></div>
-                    <br>
-                </form>
-            </div>
-
         </div>
     </div>
-</div>
-
-
-
-
-
 <!-- Popper.js first, then Bootstrap JS -->
-
 </body>
 </html>
