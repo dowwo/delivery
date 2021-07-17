12e8139798c43633ee8d84da63d12e8a209af66f
dowwo <roggers.morales@gmail.com>
2021-07-16 23:51:20 -0400

    Update login.php

--- app/Views/login.php
+++ app/Views/login.php
@@ -75,11 +75,6 @@
 
 </div>
 <div>
-    <div class="row">
-        <div class="col-md-4">.col-md-4</div>
-        <div class="col-md-4">.col-md-4</div>
-        <div class="col-md-4">.col-md-4</div>
-    </div>
     <div class="container">
         <div class="row justify-content-md-center">
 
