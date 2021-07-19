d5eda4dd89a67ef0fbc34382a23295a16f7d61a3
dowwo <roggers.morales@gmail.com>
2021-07-19 01:27:32 -0400

    Update lista_pedidos.php

--- app/Views/lista_pedidos.php
+++ app/Views/lista_pedidos.php
@@ -41,26 +41,30 @@
         } );
     </script>
     <script>
+        /* Custom filtering function which will search data in column four between two values */
+        $.fn.dataTable.ext.search.push(
+            function( settings, data, dataIndex ) {
+                var min = parseInt( $('#min').val(), 10 );
+                var max = parseInt( $('#max').val(), 10 );
+                var age = parseFloat( data[3] ) || 0; // use data for the age column
+
+                if ( ( isNaN( min ) && isNaN( max ) ) ||
+                    ( isNaN( min ) && age <= max ) ||
+                    ( min <= age   && isNaN( max ) ) ||
+                    ( min <= age   && age <= max ) )
+                {
+                    return true;
+                }
+                return false;
+            }
+        );
+
         $(document).ready(function() {
-            // Setup - add a text input to each footer cell
-            $('#lista-pedido thead tr').clone(true).appendTo( '#lista-pedido thead' );
-            $('#lista-pedido thead tr:eq(1) th').each( function (i) {
-                var title = $(this).text();
-                $(this).html( '<input type="text" placeholder="Buscar '+title+'" />' );
+            var table = $('#lista-pedido').DataTable();
 
-                $( 'input', this ).on( 'keyup change', function () {
-                    if ( table.column(i).search() !== this.value ) {
-                        table
-                            .column(i)
-                            .search( this.value )
-                            .draw();
-                    }
-                } );
-            } );
-
-            var table = $('#example').DataTable( {
-                orderCellsTop: true,
-                fixedHeader: true
+            // Event listener to the two range filtering inputs to redraw on input
+            $('#min, #max').keyup( function() {
+                table.draw();
             } );
         } );
     </script>
