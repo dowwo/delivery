af69166c3935e6b9e20a1d8158a7c1177522435a
dowwo <roggers.morales@gmail.com>
2021-07-18 23:31:43 -0400

    Update seleccionar_tienda.php

--- app/Views/seleccionar_tienda.php
+++ app/Views/seleccionar_tienda.php
@@ -48,7 +48,7 @@
             <?php endif;?>
             <form method="get">
                 <select id="list" onchange="getSelectValue();">
-                    <option disabled>Seleccione Tienda</option>
+                    <option selected="true" disabled="disabled">Seleccione</option>
                     <?php
                     foreach($tiendas as $tienda)
                     {
