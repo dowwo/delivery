183eae64823f3ceebc7c13dc9e99144642ec0571
dowwo <roggers.morales@gmail.com>
2021-07-19 01:30:48 -0400

    Update lista_pedidos.php

--- app/Views/lista_pedidos.php
+++ app/Views/lista_pedidos.php
@@ -9,10 +9,10 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
     <script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
-    
+
     <script>
         $(document).ready( function () {
-            $('#lista_producto').DataTable( {
+            $('#lista-pedido').DataTable( {
                 "language": {
                     "lengthMenu": "Mostrar _MENU_ registros por página",
                     "zeroRecords": "Lo sentimos, no se ha encontrado el registro",
