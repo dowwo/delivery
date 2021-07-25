432f1832f9f9a0c5b80f294a903c225da0f41be7
dowwo <roggers.morales@gmail.com>
2021-07-25 17:42:13 -0400

    Update ProductController.php

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -74,8 +74,8 @@ class ProductController extends Controller
         $builder = $modeloTienda->db->table("tienda");
 
         $builder->select('*');
-        $builder->where('usuario_id_usuario=' .$id_usuario);
-        $data['tiendas'] = $builder->get();
+        $builder->where('usuario_id_usuario=' .$id_usuario)->findAll();
+        $data['tiendas'] = $builder;
         return view('modificar_producto', $data);
     }
 
