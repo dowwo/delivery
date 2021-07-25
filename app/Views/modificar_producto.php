d3d9d15ebf35d1832ee8fca5623bfeccc9dea2c1
Cristofer-Sepulveda <cristofer.sepulveda02@gmail.com>
2021-07-25 18:46:28 -0400

    Update modificar_producto.php

--- app/Views/modificar_producto.php
+++ app/Views/modificar_producto.php
@@ -7,8 +7,9 @@
     <!-- Bootstrap CSS -->
 
     <title>Modificar Producto</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-    <style>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+    <!--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">-->
+    <style type="text/css">
         body {
             background-color: #AB3E5B;
         }
