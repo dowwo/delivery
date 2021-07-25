d2a3e99f5be8360ca16b890aeb3be546eae3eafe
Cristofer-Sepulveda <cristofer.sepulveda02@gmail.com>
2021-07-25 18:40:37 -0400

    Update modificar_producto.php

--- app/Views/modificar_producto.php
+++ app/Views/modificar_producto.php
@@ -2,9 +2,16 @@
 <html>
 
 <head>
-    <title>Codeigniter 4 Add User With Validation Demo</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap CSS -->
+
+    <title>Modificar Producto</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
     <style>
+        body {
+            background-color: #AB3E5B;
+        }
         .container {
             max-width: 500px;
         }
@@ -19,6 +26,29 @@
 </head>
 
 <body>
+<div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">Bienvenido <?php echo $_SESSION['nombre'] ?></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="dashboard1">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+            </ul>
+            <form class="form-inline my-2 my-lg-0">
+                <!--Botón para cerrar sesión, aplicable en cualquier parte-->
+                <a class="btn btn-outline-danger my-2 my-sm-0" href="login/logout">Cerrar sesión</a>
+            </form>
+        </div>
+    </nav>
+</div>
 <div class="container mt-5">
     <form method="post" id="add_create" name="add_create" action="<?= site_url('/ProductController/update') ?>">
         <div class="form-group">
