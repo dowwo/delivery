acd0f6209bc2f6f8284c3f74e4c0196dcab40a32
dowwo <roggers.morales@gmail.com>
2021-07-26 03:21:59 -0400

    eliminar

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -71,7 +71,7 @@ $routes->get('/agregar_pedido/(:num)','PedidoController::agregar/$1', ['filter' 
 $routes->get('/lista_pedidos','PedidoController::index', ['filter' => 'auth']);
 $routes->get('modificar_pedido/(:num)','PedidoController::singlePedido/$1');
 $routes->post('update', 'PedidoController::update');
-$routes->get('eliminar/(:num)','PedidoController::eliminar/$1');
+$routes->get('eliminar_pedido/(:num)','PedidoController::eliminar/$1');
 
 
 
