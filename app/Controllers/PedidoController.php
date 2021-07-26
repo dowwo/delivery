b3d29582abaa2c931732a04a200a52afda1b26c8
dowwo <roggers.morales@gmail.com>
2021-07-26 00:45:23 -0400

    Update PedidoController.php

--- app/Controllers/PedidoController.php
+++ app/Controllers/PedidoController.php
@@ -81,7 +81,7 @@ class PedidoController extends Controller
             return redirect()->to('/lista_pedidos');
         }else{
             $data['validation'] = $this->validation;
-            return redirect()->to('/agregar_pedido');
+            return redirect()->to('../');
         }
     }
 
