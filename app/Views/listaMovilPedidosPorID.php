606926a8b04676364337b96a079b60df817d5d0e
dowwo <roggers.morales@gmail.com>
2021-11-21 16:56:53 -0300

    Update listaMovilPedidosPorID.php

--- app/Views/listaMovilPedidosPorID.php
+++ app/Views/listaMovilPedidosPorID.php
@@ -2,8 +2,7 @@
 include('functions.php');
 //$tipo = $_GET['txtTi'];
 $array = array();
-if($resultset=getSQLResultSet("SELECT id_producto, nombre_producto, cantidad, fecha_agregado, valor, tienda_id_tienda, categoria_id_categoria 
-FROM producto, tienda, categoria")){
+if($resultset=getSQLResultSet("SELECT * FROM producto, tienda, categoria")){
 
     while ($row = $resultset->fetch_array(MYSQLI_NUM)){
         $e = array();
@@ -12,8 +11,8 @@ FROM producto, tienda, categoria")){
         $e['cantidad'] = $row[2];
         $e['fecha_agregado'] = $row[3];
         $e['valor'] = $row[4];
-        $e['nombre_tienda'] = $row[5];
-        $e['nombre_categoria'] = $row[6];
+        $e['tienda_id_tienda'] = $row[5];
+        $e['categoria_id_categoria'] = $row[6];
         array_push($array,$e);
     }
     echo json_encode($array);
