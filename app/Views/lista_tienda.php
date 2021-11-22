3a8e3ffa31af2f8868f245d41e1321427133c2db
dowwo <roggers.morales@gmail.com>
2021-11-22 19:06:13 -0300

    Update lista_tienda.php

--- app/Views/lista_tienda.php
+++ app/Views/lista_tienda.php
@@ -41,6 +41,35 @@
         } );
     </script>
     <!--NO ENTIENDO EL SCRIPT ANTERIOR :( -->
+    <!--Yo si - Dowwo-->
+    <script>
+        /* Custom filtering function which will search data in column four between two values */
+        $.fn.dataTable.ext.search.push(
+            function( settings, data, dataIndex ) {
+                var min = parseInt( $('#min').val(), 10 );
+                var max = parseInt( $('#max').val(), 10 );
+                var age = parseFloat( data[3] ) || 0; // use data for the age column
+
+                if ( ( isNaN( min ) && isNaN( max ) ) ||
+                    ( isNaN( min ) && age <= max ) ||
+                    ( min <= age   && isNaN( max ) ) ||
+                    ( min <= age   && age <= max ) )
+                {
+                    return true;
+                }
+                return false;
+            }
+        );
+
+        $(document).ready(function() {
+            var table = $('#lista-pedido').DataTable();
+
+            // Event listener to the two range filtering inputs to redraw on input
+            $('#min, #max').keyup( function() {
+                table.draw();
+            } );
+        } );
+    </script>
 </head>
 <body id="navbar_distance">
     <div>
