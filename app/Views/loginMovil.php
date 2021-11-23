0a6e2128c47dd7cc31e022cd6a100348b8e43597
dowwo <roggers.morales@gmail.com>
2021-11-23 12:10:26 -0300

    Update loginMovil.php

--- app/Views/loginMovil.php
+++ app/Views/loginMovil.php
@@ -6,8 +6,9 @@ $password = filter_input(INPUT_POST, "password");
 $rol = "47374";
 
 $sql = "SELECT email, password, rol FROM usuario WHERE (email='.$email.' and password='.$password.' and rol_id_rol='.$rol.')";
+$sql2 = "SELECT * FROM `usuario` WHERE 1";
 
-$result = getSQLResultSet($sql);
+$result = getSQLResultSet($sql2);
 echo $result;
 
 //if ($data = mysqli_fetch_array($result)){
