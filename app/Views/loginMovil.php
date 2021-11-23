c5a9c429ef1840d20824a6114134ee001fab76a6
Cristofer-Sepulveda <cristofer.sepulveda02@gmail.com>
2021-11-23 06:00:09 -0300

    Update loginMovil.php
    login appmovil (prueba)

--- app/Views/loginMovil.php
+++ app/Views/loginMovil.php
@@ -5,14 +5,12 @@ $email = filter_input(INPUT_POST, "email");
 $password = filter_input(INPUT_POST, "password");
 $rol = "47374";
 
-$sql = "SELECT email, password, rol FROM usuario WHERE (email='$email' and password='$password' and rol_id_rol='$rol')";
+$sql = "SELECT email, password, rol FROM usuario WHERE (email='.$email.' and password='.$password.' and rol_id_rol='.$rol.')";
 
 $result = ejecutarSQLCommand($sql);
-$count = mysqli_num_rows($result);
 
-if($count == 1){
-    echo '1';
-}else{
-    echo '0';
+if ($data = mysqli_fetch_array($result)){
+    echo "1";
 }
+
 ?>
