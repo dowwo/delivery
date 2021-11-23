a7e4abd98267809a3129d9dab13856d0811b31ee
Cristofer-Sepulveda <cristofer.sepulveda02@gmail.com>
2021-11-23 05:46:54 -0300

    Update loginMovil.php
    Prueba login app movil

--- app/Views/loginMovil.php
+++ app/Views/loginMovil.php
@@ -1,1 +1,18 @@
 <?php
+include('functions.php');
+
+$email = filter_input(INPUT_POST, "email");
+$password = filter_input(INPUT_POST, "password");
+$rol = "47374";
+
+$sql = "SELECT email, password, rol FROM usuario WHERE (email='$email' and password='$password' and rol_id_rol='$rol')";
+
+$result = ejecutarSQLCommand($sql);
+$count = mysqli_num_rows($result);
+
+if($count == 1){
+    echo '1';
+}else{
+    echo '0';
+}
+?>
