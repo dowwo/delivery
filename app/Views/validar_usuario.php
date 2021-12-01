5f4cdd81731b1e1d520231be6345adb377e590f1
dowwo <roggers.morales@gmail.com>
2021-12-01 16:34:37 -0300

    Update validar_usuario.php

--- app/Views/validar_usuario.php
+++ app/Views/validar_usuario.php
@@ -1,7 +1,26 @@
 <?php
-include 'conexion.php';
+
+<!--include 'conexion.php';-->
+include 'functions.php';
+
 $usu_email=$_POST['repartidor@gmail.com'];
 $usu_password=$_POST['asd123'];
+
+$array = array();
+if($resultset=getSQLResultSet("SELECT * FROM usuario WHERE email=? AND password=?")){
+
+    while ($row = $resultset->fetch_array(MYSQLI_NUM)){
+        $e = array();
+        $e['id_usuario'] = $row[0];
+        $e['nombre'] = $row[1];
+        $e['email'] = $row[2];
+        $e['password'] = $row[3];
+        array_push($array,$e);
+    }
+    echo json_encode($array);
+}
+
+
 
 $sentencia= $conexion->prepare("SELECT * FROM usuario WHERE email=? AND password=?");
 $sentencia->bind_param('ss', $usu_email, $usu_password);
@@ -13,7 +32,7 @@ if ($fila = $resultado->fetch_assoc()){
     echo "Respuesta correcta";
     echo json_encode($fila, JSON_UNESCAPED_UNICODE);
 }else{
-    echo "<br/>Error en la respuesta";
+    echo "<br/>Error en la respuesta: ";
     echo json_encode($fila, JSON_UNESCAPED_UNICODE);
 }
 $sentencia->close();
