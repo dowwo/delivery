7c006318ccd5732d55fd5d3529bbf6c151ce04df
dowwo <roggers.morales@gmail.com>
2021-12-01 16:41:48 -0300

    Update validar_usuario.php

--- app/Views/validar_usuario.php
+++ app/Views/validar_usuario.php
@@ -7,6 +7,8 @@ $usu_email=$_POST['repartidor@gmail.com'];
 $usu_password=$_POST['asd123'];
 
 $array = array();
+// Esta linea ingresa directamente lo que obtiene por POST, pero en esta caso estan directamente puestos
+/*if($resultset=getSQLResultSet("SELECT * FROM usuario WHERE email=? AND password=?")){*/
 if($resultset=getSQLResultSet("SELECT * FROM usuario WHERE email='repartidor@gmail.com' AND password='asd123'")){
 
     while ($row = $resultset->fetch_array(MYSQLI_NUM)){
