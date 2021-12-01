13a17640956ade5abae2ad0d539536cc960fdf6d
dowwo <roggers.morales@gmail.com>
2021-12-01 15:58:50 -0300

    Update validar_usuario.php

--- app/Views/validar_usuario.php
+++ app/Views/validar_usuario.php
@@ -10,6 +10,8 @@ $sentencia->execute();
 $resultado = $sentencia->get_result();
 if ($fila = $resultado->fetch_assoc()){
     echo json_encode($fila, JSON_UNESCAPED_UNICODE);
+}else{
+    echo "Respuesta";
 }
 $sentencia->close();
 $conexion->close();
