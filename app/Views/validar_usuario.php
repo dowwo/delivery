57e934fddbcf33a7ea64305cc80374a2bdc7070e
dowwo <roggers.morales@gmail.com>
2021-12-01 16:21:40 -0300

    Update validar_usuario.php

--- app/Views/validar_usuario.php
+++ app/Views/validar_usuario.php
@@ -3,7 +3,7 @@ include 'conexion.php';
 $usu_email=$_POST['roggers.morales@gmail.com'];
 $usu_password=$_POST['e10adc3949ba59abbe56e057f20f883e'];
 
-$sentencia= $conexion->prepare("SELECT * FROM usuario WHERE email='roggers.morales@gmail.com' AND password='123456'");
+$sentencia= $conexion->prepare("SELECT * FROM usuario WHERE email='repartidor@gmail.com' AND password='asd123'");
 $sentencia->bind_param('ss', $usu_email, $usu_password);
 $sentencia->execute();
 
