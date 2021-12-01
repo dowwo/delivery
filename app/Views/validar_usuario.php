381c93a14ce9239beb9fc13d2bc39237f4e7a45f
dowwo <roggers.morales@gmail.com>
2021-12-01 16:08:22 -0300

    Update validar_usuario.php

--- app/Views/validar_usuario.php
+++ app/Views/validar_usuario.php
@@ -8,6 +8,7 @@ $sentencia->bind_param('ss', $usu_email, $usu_password);
 $sentencia->execute();
 
 $resultado = $sentencia->get_result();
+echo $sentencia;
 if ($fila = $resultado->fetch_assoc()){
     echo "Respuesta correcta";
     echo json_encode($fila, JSON_UNESCAPED_UNICODE);
