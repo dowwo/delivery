5af323d5f1573968315f1aa95d898ee4e0e830de
dowwo <roggers.morales@gmail.com>
2021-12-01 13:41:12 -0300

    Update LoginMovilController.php

--- app/Controllers/LoginMovilController.php
+++ app/Controllers/LoginMovilController.php
@@ -3,9 +3,11 @@
 namespace App\Controllers;
 
 use App\Models\UserModel;
+use CodeIgniter\RESTful\ResourceController;
 use CodeIgniter\Controller;
 
-class LoginMovilController extends Controller
+
+class LoginMovilController extends ResourceController
 {
     public function index()
     {
@@ -18,4 +20,20 @@ class LoginMovilController extends Controller
         $email = $this->request->getVar('email');
         $password = $this->request->getVar('password');
     }
+
+    //Yo probare por aca - Dowwo
+
+    protected $modelName = 'app\Models\UserModel';
+    protected $format    = 'json';
+
+    public function mLogin()
+    {
+        return $this->respond($this->model->findAll());
+    }
+
+    // ...
+
+
+
+    //Hasta aca
 }
