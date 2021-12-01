a4af8ba7f30d472002dc0ef6e60917745ec54691
dowwo <roggers.morales@gmail.com>
2021-12-01 15:36:18 -0300

    login Config

---
+++ app/Views/conexion.php
@@ -0,0 +1,9 @@
+<?php
+$hostname='valefor.servidoresph.com';
+$database='deliver2_bddeliverycl';
+$username='deliver2_dcla';
+$passwrod='/-791348265-/';
+
+
+?>
+
---
+++ app/Views/validar_usuario.php
@@ -0,0 +1,17 @@
+<?php
+include 'conexion.php';
+$usu_email=$_POST['email'];
+$usu_password=$_POST['password'];
+
+$sentencia= $conexion->prepare("SELECT * FROM usuario WHERE email=? AND password=?");
+$sentencia->bind_param('ss', $usu_email, $usu_password);
+$sentencia->execute();
+
+$resultado = $sentencia->get_result();
+if ($fila = $resultado->fetch_assoc()){
+    echo json_encode($fila, JSON_UNESCAPED_UNICODE);
+}
+$sentencia->close();
+$conexion->close();
+
+?>
