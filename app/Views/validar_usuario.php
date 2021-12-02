4f83628683947b368b7c422dbd038d762843c1be
Cristofer-Sepulveda <cristofer.sepulveda02@gmail.com>
2021-12-01 23:17:26 -0300

    Update validar_usuario.php

--- app/Views/validar_usuario.php
+++ app/Views/validar_usuario.php
@@ -4,14 +4,14 @@ include 'conexion.php';
 include 'functions.php';
 
 $usu_email=$_POST['cristofer.sepulveda02@gmail.com'];
-$usu_password=$_POST[password_hash('Animexdotaku15', PASSWORD_DEFAULT)];
+$usu_password=$_POST[password_verify('Animexdotaku15', PASSWORD_DEFAULT)];
 
 $array = array();
 // Esta linea ingresa directamente lo que obtiene por POST, pero en esta caso estan directamente puestos
 /*if($resultset=getSQLResultSet("SELECT * FROM usuario WHERE email=? AND password=?")){*/
 //if($resultset=getSQLResultSet("SELECT * FROM usuario WHERE email='repartidor@gmail.com' AND password='asd123'")){
 //$query = "SELECT * FROM usuario WHERE email="+ $usu_email +" AND password="+ $usu_password;
-if($resultset=ejecutarSQLCommand("SELECT * FROM usuario WHERE email="+$usu_email+" AND password="+$usu_password)){
+if($resultset=ejecutarSQLCommand("SELECT * FROM usuario WHERE email=".$usu_email." AND password=".$usu_password)){
 
 
 
