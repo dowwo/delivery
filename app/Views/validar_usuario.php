798e0d80e6104c7f544803b0e68dc727bb5527d6
dowwo <roggers.morales@gmail.com>
2021-12-02 15:19:25 -0300

    Update validar_usuario.php

--- app/Views/validar_usuario.php
+++ app/Views/validar_usuario.php
@@ -13,15 +13,17 @@ echo $email , $pass;
 */
 
 
-$usu_email=$_POST['cristofer.sepulveda02@gmail.com'];
-$usu_password=$_POST['Animexdotaku15'];
+$usu_email=$_POST['email'];
+$usu_password=$_POST['password'];
 
 $array = array();
 // Esta linea ingresa directamente lo que obtiene por POST, pero en esta caso estan directamente puestos
 /*if($resultset=getSQLResultSet("SELECT * FROM usuario WHERE email=? AND password=?")){*/
 //if($resultset=getSQLResultSet("SELECT * FROM usuario WHERE email='repartidor@gmail.com' AND password='asd123'")){
 //$query = "SELECT * FROM usuario WHERE email="+ $usu_email +" AND password="+ $usu_password;
-if($resultset=getSQLResultSet("SELECT id_usuario, email, password, rol_id_rol FROM usuario WHERE email='dowwo@gmail.com' AND password='123456'")){
+//if($resultset=getSQLResultSet("SELECT id_usuario, email, password, rol_id_rol FROM usuario WHERE email='dowwo@gmail.com' AND password='123456'")){
+/*
+if($resultset=getSQLResultSet("SELECT id_usuario, email, password, rol_id_rol FROM usuario WHERE email=? AND password=?")){
 
 
 
@@ -36,5 +38,17 @@ if($resultset=getSQLResultSet("SELECT id_usuario, email, password, rol_id_rol FR
     }
     echo json_encode($array);
 }
+*/
+
+$sentencia = $conexion->prepare("SELECT * FROM usuario WHERE email=? AND password=?");
+$sentencia->bind_param('ss',$usu_email, $usu_password);
+$sentencia->execute();
+
+$resultado = $sentencia->get_result();
+if ($fila = $resultado->fetch_assoc()){
+    echo json_encode($fila, JSON_UNESCAPED_UNICODE);
+}
+$sentencia->close();
+$conexion->close();
 
 ?>
