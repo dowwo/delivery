0cc16850d0e7d402c835f4675741d99b1b8fcd11
dowwo <roggers.morales@gmail.com>
2021-12-02 00:05:22 -0300

    Update validar_usuario.php

--- app/Views/validar_usuario.php
+++ app/Views/validar_usuario.php
@@ -21,7 +21,7 @@ $array = array();
 /*if($resultset=getSQLResultSet("SELECT * FROM usuario WHERE email=? AND password=?")){*/
 //if($resultset=getSQLResultSet("SELECT * FROM usuario WHERE email='repartidor@gmail.com' AND password='asd123'")){
 //$query = "SELECT * FROM usuario WHERE email="+ $usu_email +" AND password="+ $usu_password;
-if($resultset=ejecutarSQLCommand("SELECT * FROM usuario WHERE email='cristofer.sepulveda02@gmail.com' AND password='Animexdotaku15'")){
+if($resultset=ejecutarSQLCommand("SELECT * FROM usuario WHERE email='dowwo@gmail.com' AND password='123456'")){
 
 
 
