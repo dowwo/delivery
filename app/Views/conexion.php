d4c93588ba6219e3bb5bbd44f5db0aba2ea7889c
dowwo <roggers.morales@gmail.com>
2021-12-02 15:44:07 -0300

    Update conexion.php

--- app/Views/conexion.php
+++ app/Views/conexion.php
@@ -4,7 +4,7 @@ $database='deliver2_bddeliverycl';
 $username='deliver2_appmovil';
 $password='A7s4D1F8g5';
 
-echo $hostname, $database, $username, $password;
+
 
 $conexion= new mysqli($hostname, $username, $password, $database);
 if ($conexion->connect_errno){
