769cab94488b6991991ff1c78458b5480c29a250
dowwo <roggers.morales@gmail.com>
2021-12-02 14:08:06 -0300

    Update Login.php

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -19,9 +19,28 @@ class Login extends Controller
         $password = $this->request->getVar('password');
 
         $data = $model->where('email', $email)->first();
+        /**/
         if($data){
 
             $pass = $data['password'];
+
+            $verify_pass = ($pass);
+            if($verify_pass){
+                $ses_data = [
+
+                    'id_usuario'    => $data['id_usuario'],
+                    'nombre'        => $data['nombre'],
+                    'email'         => $data['email'],
+                    'rol_id_rol'    => $data['rol_id_rol'],
+                    'logged_in'     => TRUE
+                ];
+                $session->set($ses_data);
+                return redirect()->to('/dashboard1');
+            }else{
+                $session->setFlashdata('msg', 'Contraseña incorrecta');
+                return redirect()->to('/login');
+            }
+            /*
             $verify_pass = password_verify($password, $pass);
             if($verify_pass){
                 $ses_data = [
@@ -38,6 +57,7 @@ class Login extends Controller
                 $session->setFlashdata('msg', 'Contraseña incorrecta');
                 return redirect()->to('/login');
             }
+            */
         }else{
             $session->setFlashdata('msg', 'Email no encontrado');
             return redirect()->to('/login');
