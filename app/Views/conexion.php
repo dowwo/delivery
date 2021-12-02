2dd60d38ee9d0e39d2d1781e3e44133be46fd810
dowwo <roggers.morales@gmail.com>
2021-12-02 15:43:41 -0300

    Update conexion.php

--- app/Views/conexion.php
+++ app/Views/conexion.php
@@ -4,6 +4,8 @@ $database='deliver2_bddeliverycl';
 $username='deliver2_appmovil';
 $password='A7s4D1F8g5';
 
+echo $hostname, $database, $username, $password;
+
 $conexion= new mysqli($hostname, $username, $password, $database);
 if ($conexion->connect_errno){
     echo "El sitio web esta experimentando problemas";
