ae3856c90b8fb642971b355baac2f57f6db065a1
dowwo <roggers.morales@gmail.com>
2021-12-01 23:54:00 -0300

    Update validar_usuario.php

--- app/Views/validar_usuario.php
+++ app/Views/validar_usuario.php
@@ -3,22 +3,31 @@
 include 'conexion.php';
 include 'functions.php';
 
-$email = 'cristofer.sepulveda02@gmail.com';
-$pass = 'Animexdotaku15';
 
-echo $email;
-echo $pass;
+$usu_email = $_POST['cristofer.sepulveda02@gmail.com'];
+$usu_password = $_POST['Animexdotaku15'];
 
+//$usu_email = "cristofer.sepulveda02@gmail.com";
+//$usu_password = "Animexdotaku15";
 
+$sentencia =  $conexion->prepare("
+    SELECT * FROM usuario WHERE email=? AND password=?");
+$sentencia->bind_param('ss', $usu_email, $usu_password);
+$sentencia->execute();
 
-$usu_email=$_GET['cristofer.sepulveda02@gmail.com'];
-$usu_password=$_GET[password_hash('Animexdotaku15', PASSWORD_DEFAULT)];
+$resultado = $sentencia->get_result();
+if ($fila = $resultado->fetch_assoc()){
+    echo json_encode($fila, JSON_UNESCAPED_UNICODE);
+}
+$sentencia->close();
+$conexion->close();
 
-$array = array();
+//$array = array();
 // Esta linea ingresa directamente lo que obtiene por POST, pero en esta caso estan directamente puestos
 /*if($resultset=getSQLResultSet("SELECT * FROM usuario WHERE email=? AND password=?")){*/
 //if($resultset=getSQLResultSet("SELECT * FROM usuario WHERE email='repartidor@gmail.com' AND password='asd123'")){
 //$query = "SELECT * FROM usuario WHERE email="+ $usu_email +" AND password="+ $usu_password;
+/*
 if($resultset=getSQLResultSet("SELECT * FROM usuario WHERE email=".$email." AND password=".$pass)){
 
 
@@ -37,6 +46,6 @@ if($resultset=getSQLResultSet("SELECT * FROM usuario WHERE email=".$email." AND 
     echo $usu_email;
     echo $usu_password;
     echo json_encode($array);
-}
+}*/
 
 ?>
