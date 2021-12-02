28c1064490c83f68957c60269850f017cefa335e
dowwo <roggers.morales@gmail.com>
2021-12-02 15:47:53 -0300

    Update validar_usuario.php

--- app/Views/validar_usuario.php
+++ app/Views/validar_usuario.php
@@ -1,27 +1,11 @@
 <?php
-use CodeIgniter\Controller;
-use App\Models\UserModel;
 
 include 'conexion.php';
-include 'functions.php';
-/*
-$email = 'cristofer.sepulveda02@gmail.com';
-$data = $model->where('email', $email)->first();
-$pass = password_verify('Animexdotaku15', $data);
-
-echo $email , $pass;
-*/
-
 
 $usu_email=$_POST['email'];
 $usu_password=$_POST['password'];
 
 $array = array();
-// Esta linea ingresa directamente lo que obtiene por POST, pero en esta caso estan directamente puestos
-/*if($resultset=getSQLResultSet("SELECT * FROM usuario WHERE email=? AND password=?")){*/
-//if($resultset=getSQLResultSet("SELECT * FROM usuario WHERE email='repartidor@gmail.com' AND password='asd123'")){
-//$query = "SELECT * FROM usuario WHERE email="+ $usu_email +" AND password="+ $usu_password;
-//if($resultset=getSQLResultSet("SELECT id_usuario, email, password, rol_id_rol FROM usuario WHERE email='dowwo@gmail.com' AND password='123456'")){
 /*
 if($resultset=getSQLResultSet("SELECT id_usuario, email, password, rol_id_rol FROM usuario WHERE email=? AND password=?")){
 
