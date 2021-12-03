4108694b7f927bd354522bbb0876a7df8a534f36
Cristofer-Sepulveda <cristofer.sepulveda02@gmail.com>
2021-12-02 22:49:38 -0300

    new dash

--- app/Views/dashboard1.php
+++ app/Views/dashboard1.php
@@ -1,8 +1,8 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo base_url('public/img/apple-icon.png')?>">
-    <link rel="icon" type="image/png" href="<?php echo base_url('public/img/favicon.png')?>">
+    <link rel="apple-touch-icon" sizes="76x76" href="../extras/img/apple-icon.png">
+    <link rel="icon" type="image/png" href="../extras/img/favicon.png">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title>Delivery Chile</title>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
