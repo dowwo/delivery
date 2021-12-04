3504796a15983783a84077601cca11e7aeb8b2e0
dowwo <roggers.morales@gmail.com>
2021-12-04 20:31:26 -0300

    Update validar_usuario.php

--- app/Views/validar_usuario.php
+++ app/Views/validar_usuario.php
@@ -6,7 +6,7 @@ $usu_email=$_POST['email'];
 $usu_password=$_POST['password'];
 
 
-$sentencia=$conexion->prepare("SELECT * FROM usuario WHERE email=? AND password=?");
+$sentencia=$conexion->prepare("SELECT id_usuario, nombre, email, password, rol_id_rol  FROM usuario WHERE email=? AND password=?");
 $sentencia->bind_param('ss', $usu_email, $usu_password);
 $sentencia->execute();
 
