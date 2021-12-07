f299b2754f617b75b5c761d1530e17f92194d310
dowwo <roggers.morales@gmail.com>
2021-12-07 01:18:59 -0300

    Update validar_usuario.php

--- app/Views/validar_usuario.php
+++ app/Views/validar_usuario.php
@@ -4,6 +4,7 @@
 include 'conexion.php';
 $usu_email=$_POST['email'];
 $usu_password=$_POST['password'];
+$datos ="";
 
 
 $sentencia=$conexion->prepare("SELECT id_usuario, nombre, email, password, rol_id_rol  FROM usuario WHERE email=? AND password=?");
@@ -13,7 +14,8 @@ $sentencia->execute();
 $resultado = $sentencia->get_result();
 
 if ($fila = $resultado->fetch_assoc()) {
-    echo json_encode($fila).str_replace("\n","","");
+    $datos = json_encode($fila).str_replace("\n", "", "$datos");
+    echo $datos;
 }
 $sentencia->close();
 $conexion->close();
