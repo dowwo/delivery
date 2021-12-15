64a2cab2ab031e4ea3742fd717183b001fd84179
dowwo <roggers.morales@gmail.com>
2021-12-15 16:45:11 -0300

    Update agregar_pedido.php

--- app/Views/agregar_pedido.php
+++ app/Views/agregar_pedido.php
@@ -154,7 +154,7 @@ if(isset($_SESSION['msg'])){
 <!--Este script es para utilizar el autocompletado y validacion de direcciones de google maps-->
 <!--AIzaSyBp3qUeUUevPEBWY1v-3dJJs8yEgtNrP7ILa api que utiliza el sitio web es la de Places, por lo que debe habilitarse aparte en la cuenta de google-->
 <script src="https://maps.googleapis.com/maps/api/js?
-key=API_KEY&libraries=places&callback=initAutocomplete" async defer>
+key=AIzaSyBp3qUeUUevPEBWY1v-3dJJs8yEgtNrP7ILa&libraries=places&callback=initAutocomplete" async defer>
 </script>
 
 
