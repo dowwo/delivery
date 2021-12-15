250dfcb4a4bbe29d628c8db9186151c394e0177d
dowwo <roggers.morales@gmail.com>
2021-12-15 16:41:31 -0300

    Update agregar_pedido.php

--- app/Views/agregar_pedido.php
+++ app/Views/agregar_pedido.php
@@ -90,8 +90,59 @@ if(isset($_SESSION['msg'])){
                     <label for="InputForDireccion" class="form-label">Dirección destino</label>
                     <input type="text" name="direccion" class="form-control" id="InputForDireccion">
                 </div>
+
                 <!-- No lo recordaba pero tenia comentado los campos latitud y longitud para utilizarlos luego xd -->
                 <div>
+                    <?php
+                    if(isset($_POST['submit_address']))
+                    {
+                        $address =$_POST['address']; // Google HQ
+                        $prepAddr = str_replace(' ','+',$address);
+                        $geocode=file_get_contents('http://maps.google.com/maps/api/geocode/json?address='.$prepAddr.'&sensor=false');
+                        $output= json_decode($geocode);
+                        $latitude = $output->results[0]->geometry->location->lat;
+                        $longitude = $output->results[0]->geometry->location->lng;
+
+                        echo "latitude - ".$latitude;
+                        echo "longitude - ".$longitude;
+                    }
+
+                    if(isset($_POST['submit_coordinates']))
+                    {
+                        $lat=$_POST['latitude'];
+                        $long=$_POST['longitude'];
+
+                        $url  = "http://maps.googleapis.com/maps/api/geocode/json?latlng=".$lat.",".$long."&sensor=false";
+                        $json = @file_get_contents($url);
+                        $data = json_decode($json);
+                        $status = $data->status;
+                        $address = '';
+                        if($status == "OK")
+                        {
+                            echo $address = $data->results[0]->formatted_address;
+                        }
+                        else
+                        {
+                            echo "No Data Found Try Again";
+                        }
+                    }
+                    ?>
+                    <form method="post" action='get_data.php'>
+                        <p>Enter Your Street name,City state,Country</p>
+                        <textarea name='address' placeholder='Street name,City state,Country'></textarea>
+                        <input type="submit" name="submit_address" value="Get Coordinates">
+                    </form>
+                    <form method="post" action='get_data.php'>
+                        <p>Enter Latitude</p>
+                        <input type='text' name='latitude' placeholder='Enter Latitude'>
+                        <p>Enter Longitude</p>
+                        <input type='text' name='longitude' placeholder='Enter Longitude'>
+                        <input type="submit" name="submit_coordinates" value="Get Address">
+                    </form>
+
+
+
+
                     <label for="InputForLatitud" class="form-label">Latitud</label>
                     <input id="autocomplete" placeholder="Enter a place" type="text">
                     <input type="text" name="latitud" class="form-control" id="InputForLatitud">
