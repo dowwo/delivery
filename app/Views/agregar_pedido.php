b18e8b48d590d636e09989ce5ec9d78c774e4af9
dowwo <roggers.morales@gmail.com>
2021-12-16 01:22:04 -0300

    Update agregar_pedido.php

--- app/Views/agregar_pedido.php
+++ app/Views/agregar_pedido.php
@@ -127,8 +127,13 @@
                 // Displaying the value
                 alert(inputLat);
 
+                $("#InputForLatitud").val(evt.latLng.lat().toFixed(6));
+                $("#InputForLongitud").val(evt.latLng.lng().toFixed(6));
+                /*
                 inputLatitud.val(evt.latLng.lat().toFixed(6));
                 inputLongitud.val(evt.latLng.lng().toFixed(6));
+                */
+                 */
 
                 document.write('<div></div>');
 
