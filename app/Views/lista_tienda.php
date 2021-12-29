0ff7d67e1ff1e33d8f1d224f97e5eca1a6ec827d
dowwo <roggers.morales@gmail.com>
2021-12-29 16:39:34 -0300

    Update lista_tienda.php

--- app/Views/lista_tienda.php
+++ app/Views/lista_tienda.php
@@ -399,34 +399,5 @@
 </div>
 
 
-
-
-
-
-    <div>
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <a class="navbar-brand" href="#">Bienvenido <?php echo $_SESSION['nombre'] ?></a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="dashboard1">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                </ul>
-                <form class="form-inline my-2 my-lg-0">
-                    <!--Botón para cerrar sesión, aplicable en cualquier parte-->
-                    <a class="btn btn-outline-danger my-2 my-sm-0" href="login/logout">Cerrar sesión</a>
-                </form>
-            </div>
-        </nav>
-    </div>
-
-
 </body>
 </html>
