4a3eeec3ed3a7fceedb0013e1cffc000200f75a2
dowwo <roggers.morales@gmail.com>
2021-12-29 15:34:14 -0300

    Update agregar_pedido.php

--- app/Views/agregar_pedido.php
+++ app/Views/agregar_pedido.php
@@ -450,24 +450,14 @@ if(isset($_SESSION['msg'])){
             <!-- Esta NAVBAR será la que mueva al usuario entre opciones-->
             <li class="active">
                 <a href="../lista_pedidos">Ver Pedidos</a>
+
+            </li>
+            <li>
                 
             </li>
             <li>
-                <a href="#">About</a>
-            </li>
-            <li>
                 <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
-                <ul class="collapse list-unstyled" id="pageSubmenu">
-                    <li>
-                        <a href="#">Page 1</a>
-                    </li>
-                    <li>
-                        <a href="#">Page 2</a>
-                    </li>
-                    <li>
-                        <a href="#">Page 3</a>
-                    </li>
-                </ul>
+
             </li>
             <li>
                 <a href="#">Portfolio</a>
