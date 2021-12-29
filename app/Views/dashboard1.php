f33e82d4de7dc97bea0a84829b754d0a58b9e947
dowwo <roggers.morales@gmail.com>
2021-12-29 15:55:38 -0300

    Update dashboard1.php

--- app/Views/dashboard1.php
+++ app/Views/dashboard1.php
@@ -23,6 +23,13 @@
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
     <script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
     <script>
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+    </script>
+    <script>
         $(document).ready( function () {
             $('#users-list').DataTable( {
                 "language": {
