5599a47abdc033dc4f52d4cc7371dc415a8fa038
dowwo <roggers.morales@gmail.com>
2021-12-29 16:17:43 -0300

    Update dashboard1.php

--- app/Views/dashboard1.php
+++ app/Views/dashboard1.php
@@ -223,29 +223,7 @@
     </style>
 </head>
 
-<body id="navbar_distance">
-<div>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Bienvenido <?php echo $_SESSION['nombre'] ?></a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-                <!--
-                <li class="nav-item active">
-                    <a class="nav-link" href="#">INICIO <span class="sr-only">(current)</span></a>
-                </li>
-                -->
-            </ul>
-            <form class="form-inline my-2 my-lg-0">
-                <!--Botón para cerrar sesión, aplicable en cualquier parte-->
-                <a class="btn btn-outline-danger my-2 my-sm-0" href="login/logout">Cerrar sesión</a>
-            </form>
-        </div>
-    </nav>
-</div>
+<body>
 
 <?php
 if(isset($_SESSION['msg'])){
