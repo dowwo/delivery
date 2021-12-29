ba0ece87f3bba5a8e8d3442c23a6261466352009
dowwo <roggers.morales@gmail.com>
2021-12-29 12:35:48 -0300

    Update agregar_pedido.php

--- app/Views/agregar_pedido.php
+++ app/Views/agregar_pedido.php
@@ -369,9 +369,9 @@ if(isset($_SESSION['msg'])){
                     <br>
                 </div>
 
-                <!--
+
                 <script async="async" defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBp3qUeUUevPEBWY1v-3dJJs8yEgtNrP7I&libraries=places&callback=initAutocomplete" >
-                </script>-->
+                </script>
                 <div id="googleMap" style="width:100%;height:400px;"></div>
 
                 <div class="mb-3">
