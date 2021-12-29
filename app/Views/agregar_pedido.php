54638d6722a453e4d92aaf570c3d2aa1ae558d72
dowwo <roggers.morales@gmail.com>
2021-12-29 12:13:48 -0300

    Update agregar_pedido.php

--- app/Views/agregar_pedido.php
+++ app/Views/agregar_pedido.php
@@ -178,6 +178,8 @@
                 setMapOnAll(null);
             }
             function deleteMarkers() {
+                document.getElementById('InputForLatitud').value = "";
+                document.getElementById('InputForLongitud').value = "";
                 hideMarkers();
                 markers = [];
             }
@@ -364,7 +366,7 @@ if(isset($_SESSION['msg'])){
                     <input type="text" name="latitud" class="form-control" id="InputForLatitud" required="required">
                     <label for="InputForLongitud" class="form-label">Longitud</label>
                     <input type="text" name="longitud" class="form-control" id="InputForLongitud" required="required">
-                    <input id="delete-markers" type="button" value="Delete Markers" class="btn btn-danger"/>
+                    <input id="delete-markers" type="button" value="Reiniciar dirección" class="btn btn-danger"/>
                     <br>
                 </div>
 
