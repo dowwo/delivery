effc60a9e123777c8dd2ac183944dfb5254534f0
dowwo <roggers.morales@gmail.com>
2021-12-30 13:54:45 -0300

    css usuarios

--- app/Views/modificar_usuario.php
+++ app/Views/modificar_usuario.php
@@ -2,60 +2,313 @@
 <html>
 
 <head>
-    <title>Codeigniter 4 Add User With Validation Demo</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <!-- Este es el que usaremos con bootstrap 3.3.0-->
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+    <!------ Include the above in your HEAD tag ---------->
+
+    <link href="https://fonts.googleapis.com/css?family=Oleo+Script:400,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Teko:400,700" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+
+    <title>Modificar Usuario</title>
+
     <style>
-        .container {
-            max-width: 500px;
+        body {
+            background-color: #AB3E5B;
+        }
+        @import "https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700";
+        body {
+            font-family: 'Poppins', sans-serif;
+            background: #fafafa;
         }
 
-        .error {
+        p {
+            font-family: 'Poppins', sans-serif;
+            font-size: 1.1em;
+            font-weight: 300;
+            line-height: 1.7em;
+            color: #999;
+        }
+
+        a,
+        a:hover,
+        a:focus {
+            color: inherit;
+            text-decoration: none;
+            transition: all 0.3s;
+        }
+
+        .navbar {
+            padding: 15px 10px;
+            background: #fff;
+            border: none;
+            border-radius: 0;
+            margin-bottom: 40px;
+            box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.1);
+        }
+
+        .navbar-btn {
+            box-shadow: none;
+            outline: none !important;
+            border: none;
+        }
+
+        .line {
+            width: 100%;
+            height: 1px;
+            border-bottom: 1px dashed #ddd;
+            margin: 40px 0;
+        }
+
+        /* ---------------------------------------------------
+            SIDEBAR STYLE
+        ----------------------------------------------------- */
+
+        .wrapper {
+            display: flex;
+            width: 100%;
+            align-items: stretch;
+        }
+
+        #sidebar {
+            min-width: 250px;
+            max-width: 250px;
+            background: #7386D5;
+            color: #fff;
+            transition: all 0.3s;
+        }
+
+        #sidebar.active {
+            margin-left: -250px;
+        }
+
+        #sidebar .sidebar-header {
+            padding: 20px;
+            background: #6d7fcc;
+        }
+
+        #sidebar ul.components {
+            padding: 20px 0;
+            border-bottom: 1px solid #47748b;
+        }
+
+        #sidebar ul p {
+            color: #fff;
+            padding: 10px;
+        }
+
+        #sidebar ul li a {
+            padding: 10px;
+            font-size: 1.1em;
             display: block;
-            padding-top: 5px;
-            font-size: 14px;
-            color: red;
+        }
+
+        #sidebar ul li a:hover {
+            color: #7386D5;
+            background: #fff;
+        }
+
+        #sidebar ul li.active>a,
+        a[aria-expanded="true"] {
+            color: #fff;
+            background: #6d7fcc;
+        }
+
+        a[data-toggle="collapse"] {
+            position: relative;
+        }
+
+        .dropdown-toggle::after {
+            display: block;
+            position: absolute;
+            top: 50%;
+            right: 20px;
+            transform: translateY(-50%);
+        }
+
+        ul ul a {
+            font-size: 0.9em !important;
+            padding-left: 30px !important;
+            background: #6d7fcc;
+        }
+
+        ul.CTAs {
+            padding: 20px;
+        }
+
+        ul.CTAs a {
+            text-align: center;
+            font-size: 0.9em !important;
+            display: block;
+            border-radius: 5px;
+            margin-bottom: 5px;
+        }
+
+        a.download {
+            background: #fff;
+            color: #7386D5;
+        }
+
+        a.article,
+        a.article:hover {
+            background: #6d7fcc !important;
+            color: #fff !important;
+        }
+
+        /* ---------------------------------------------------
+            CONTENT STYLE
+        ----------------------------------------------------- */
+
+        #content {
+            width: 100%;
+            padding: 20px;
+            min-height: 100vh;
+            transition: all 0.3s;
+        }
+
+        /* ---------------------------------------------------
+            MEDIAQUERIES
+        ----------------------------------------------------- */
+
+        @media (max-width: 768px) {
+            #sidebar {
+                margin-left: -250px;
+            }
+            #sidebar.active {
+                margin-left: 0;
+            }
+            #sidebarCollapse span {
+                display: none;
+            }
         }
     </style>
+    <script>
+        $(document).ready(function () {
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').toggleClass('active');
+            });
+        });
+    </script>
+
 </head>
 
 <body>
-<div class="container mt-5">
-    <h1>Modificar Usuario</h1>
-    <form method="post" id="add_create" name="add_create" action="<?= site_url('/UsuarioController/update') ?>">
-        <input name="id_usuario" value="<?php echo $usuario_obj['id_usuario']; ?>">
-        <div class="form-group">
-            <label>Nombre</label>
-            <input type="text" name="name" class="form-control" value="<?php echo $usuario_obj['nombre']; ?>">
-        </div>
-        <div class="form-group">
-            <label>Apellido Paterno</label>
-            <input type="text" name="apellido_p" class="form-control" value="<?php echo $usuario_obj['apellido_p']; ?>">
-        </div>
-        <div class="form-group">
-            <label>Apellido Materno</label>
-            <input type="text" name="apellido_m" class="form-control" value="<?php echo $usuario_obj['apellido_m']; ?>">
-        </div>
-        <div class="form-group">
-            <label>Email</label>
-            <input type="text" name="email" class="form-control" value="<?php echo $usuario_obj['email']; ?>">
-        </div>
-        <div class="mb-3">
-            <label for="InputForComuna" class="form-label">Tipo usuario</label>
-            <select name="rol" id="rol" class="form-select" aria-label="Default select example">
-                <?php
-                foreach($rols as $rol)
-                {
-                    ?>
-                    <option value="<?=$rol['id_rol']?>"><?=$rol['rol']?></option>
-                    <?php
-                }
-                ?>
-            </select>
-        </div>
-        <div class="form-group">
-            <button type="submit" class="btn btn-primary btn-block">Update Data</button>
-        </div>
-    </form>
+
+<div>
+    <div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div class="sidebar-header">
+                <h3>Bienvenido <?php echo $_SESSION['nombre'] ?>
+                </h3>
+            </div>
+            <ul class="list-unstyled components">
+                <li>
+                    <?php if($_SESSION['rol_id_rol'] == 47174): ?>
+                        <a href="dashboard3" class="card-link">Administrador</a>
+                    <?php else: ?>
+                        <!-- Admin link goes here -->
+                    <?php endif; ?>
+                </li>
+                <li>
+                    <a href="/dashboard1">Inicio</a>
+                </li>
+                <li>
+                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Tienda</a>
+                    <ul class="collapse list-unstyled" id="pageSubmenu">
+                        <li>
+                            <!--Redireccionar al agregar_tienda-->
+                            <a href="agregar_tienda" class="card-link">Agregar tienda</a>
+                        </li>
+                        <li>
+                            <!--Redireccionar al listar_tienda-->
+                            <a href="lista_tienda" class="card-link">Listar Tienda</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#pageSubmenu2" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pedidos</a>
+                    <ul class="collapse list-unstyled" id="pageSubmenu2">
+                        <li>
+                            <!--Redireccionar al agregar_tienda-->
+                            <a href="seleccionar_tienda" class="card-link">Agregar Pedido</a>
+                        </li>
+                        <li>
+                            <!--Redireccionar al listar_tienda-->
+                            <a href="lista_pedidos" class="card-link">Ver Pedidos</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a class="btn btn-outline-danger my-2 my-sm-0" href="../login/logout">Cerrar sesión</a>
+                </li>
+            </ul>
+        </nav>
+
+        <!-- Page Content  -->
+        <div id="content">
+
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+
+                    <button type="button" id="sidebarCollapse" class="btn btn-info">
+                        <i class="fas fa-align-left"></i>
+                        <span>Ver/Ocultar Menu</span>
+                    </button>
+
+                </div>
+            </nav>
+
+            <div class="container mt-5">
+                <h1>Modificar Usuario</h1>
+                <form method="post" id="add_create" name="add_create" action="<?= site_url('/UsuarioController/update') ?>">
+                    <input name="id_usuario" value="<?php echo $usuario_obj['id_usuario']; ?>">
+                    <div class="form-group">
+                        <label>Nombre</label>
+                        <input type="text" name="name" class="form-control" value="<?php echo $usuario_obj['nombre']; ?>">
+                    </div>
+                    <div class="form-group">
+                        <label>Apellido Paterno</label>
+                        <input type="text" name="apellido_p" class="form-control" value="<?php echo $usuario_obj['apellido_p']; ?>">
+                    </div>
+                    <div class="form-group">
+                        <label>Apellido Materno</label>
+                        <input type="text" name="apellido_m" class="form-control" value="<?php echo $usuario_obj['apellido_m']; ?>">
+                    </div>
+                    <div class="form-group">
+                        <label>Email</label>
+                        <input type="text" name="email" class="form-control" value="<?php echo $usuario_obj['email']; ?>">
+                    </div>
+                    <div class="mb-3">
+                        <label for="InputForComuna" class="form-label">Tipo usuario</label>
+                        <select name="rol" id="rol" class="form-select" aria-label="Default select example">
+                            <?php
+                            foreach($rols as $rol)
+                            {
+                                ?>
+                                <option value="<?=$rol['id_rol']?>"><?=$rol['rol']?></option>
+                                <?php
+                            }
+                            ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <button type="submit" class="btn btn-primary btn-block">Update Data</button>
+                    </div>
+                </form>
+
+
+
+            </div>
+    </div>
+</div>
+
 </div>
 
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
@@ -76,12 +329,12 @@
             },
             messages: {
                 name: {
-                    required: "Name is required.",
+                    required: "Debe ingresar un nombre.",
                 },
                 email: {
-                    required: "Email is required.",
-                    email: "It does not seem to be a valid email.",
-                    maxlength: "The email should be or equal to 60 chars.",
+                    required: "Debe ingresar un email.",
+                    email: "Debe ingresar un email válido.",
+                    maxlength: "el email no debe sar mayor a 60 caractéres.",
                 },
             },
         })
