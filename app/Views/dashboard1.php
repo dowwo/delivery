5d4073c230e2a7a26676d9a456028a39d5878fe2
dowwo <roggers.morales@gmail.com>
2021-12-30 02:52:28 -0300

    Update dashboard1.php

--- app/Views/dashboard1.php
+++ app/Views/dashboard1.php
@@ -18,10 +18,10 @@
     <link href="https://fonts.googleapis.com/css?family=Teko:400,700" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 
-    <!-- Resources para amcharts 5 -->
-    <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
-    <script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
-    <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+    <!-- Resources amcharts 4 -->
+    <script src="https://cdn.amcharts.com/lib/4/core.js"></script>
+    <script src="https://cdn.amcharts.com/lib/4/charts.js"></script>
+    <script src="https://cdn.amcharts.com/lib/4/themes/animated.js"></script>
     
     
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
@@ -235,104 +235,97 @@
 
     <!-- Chart code -->
     <script>
-        am5.ready(function() {
+        am4core.ready(function() {
 
-// Create root element
-// https://www.amcharts.com/docs/v5/getting-started/#Root_element
-            var root = am5.Root.new("chartdiv");
+// Themes begin
+            am4core.useTheme(am4themes_animated);
+// Themes end
 
+// Create chart instance
+            var chart = am4core.create("chartdiv", am4charts.XYChart);
+            chart.scrollbarX = new am4core.Scrollbar();
 
-// Set themes
-// https://www.amcharts.com/docs/v5/concepts/themes/
-            root.setThemes([
-                am5themes_Animated.new(root)
-            ]);
-
-
-// Create chart
-// https://www.amcharts.com/docs/v5/charts/xy-chart/
-            var chart = root.container.children.push(am5xy.XYChart.new(root, {
-                panX: true,
-                panY: true,
-                wheelX: "panX",
-                wheelY: "zoomX"
-            }));
-
-// Add cursor
-// https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
-            var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {}));
-            cursor.lineY.set("visible", false);
-
+// Add data
+            chart.data = [{
+                "country": "USA",
+                "visits": 3025
+            }, {
+                "country": "China",
+                "visits": 1882
+            }, {
+                "country": "Japan",
+                "visits": 1809
+            }, {
+                "country": "Germany",
+                "visits": 1322
+            }, {
+                "country": "UK",
+                "visits": 1122
+            }, {
+                "country": "France",
+                "visits": 1114
+            }, {
+                "country": "India",
+                "visits": 984
+            }, {
+                "country": "Spain",
+                "visits": 711
+            }, {
+                "country": "Netherlands",
+                "visits": 665
+            }, {
+                "country": "Russia",
+                "visits": 580
+            }, {
+                "country": "South Korea",
+                "visits": 443
+            }, {
+                "country": "Canada",
+                "visits": 441
+            }];
 
 // Create axes
-// https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
-            var xRenderer = am5xy.AxisRendererX.new(root, { minGridDistance: 30 });
-            xRenderer.labels.template.setAll({
-                rotation: -90,
-                centerY: am5.p50,
-                centerX: am5.p100,
-                paddingRight: 15
-            });
+            var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
+            categoryAxis.dataFields.category = "country";
+            categoryAxis.renderer.grid.template.location = 0;
+            categoryAxis.renderer.minGridDistance = 30;
+            categoryAxis.renderer.labels.template.horizontalCenter = "right";
+            categoryAxis.renderer.labels.template.verticalCenter = "middle";
+            categoryAxis.renderer.labels.template.rotation = 270;
+            categoryAxis.tooltip.disabled = true;
+            categoryAxis.renderer.minHeight = 110;
 
-            var xAxis = chart.xAxes.push(am5xy.CategoryAxis.new(root, {
-                maxDeviation: 0.3,
-                categoryField: "monthname(fecha_pedido)",
-                renderer: xRenderer,
-                tooltip: am5.Tooltip.new(root, {})
-            }));
-
-            var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
-                maxDeviation: 0.3,
-                renderer: am5xy.AxisRendererY.new(root, {})
-            }));
-
+            var valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+            valueAxis.renderer.minWidth = 50;
 
 // Create series
-// https://www.amcharts.com/docs/v5/charts/xy-chart/series/
-            var series = chart.series.push(am5xy.ColumnSeries.new(root, {
-                name: "Series 1",
-                xAxis: xAxis,
-                yAxis: yAxis,
-                valueYField: "SUM(valor_total)",
-                sequencedInterpolation: true,
-                categoryXField: "MONTHNAME(fecha_pedido)",
-                tooltip: am5.Tooltip.new(root, {
-                    labelText:"{valueY}"
-                })
-            }));
+            var series = chart.series.push(new am4charts.ColumnSeries());
+            series.sequencedInterpolation = true;
+            series.dataFields.valueY = "visits";
+            series.dataFields.categoryX = "country";
+            series.tooltipText = "[{categoryX}: bold]{valueY}[/]";
+            series.columns.template.strokeWidth = 0;
 
-            series.columns.template.setAll({ cornerRadiusTL: 5, cornerRadiusTR: 5 });
-            series.columns.template.adapters.add("fill", (fill, target) => {
-                return chart.get("colors").getIndex(series.columns.indexOf(target));
+            series.tooltip.pointerOrientation = "vertical";
+
+            series.columns.template.column.cornerRadiusTopLeft = 10;
+            series.columns.template.column.cornerRadiusTopRight = 10;
+            series.columns.template.column.fillOpacity = 0.8;
+
+// on hover, make corner radiuses bigger
+            var hoverState = series.columns.template.column.states.create("hover");
+            hoverState.properties.cornerRadiusTopLeft = 0;
+            hoverState.properties.cornerRadiusTopRight = 0;
+            hoverState.properties.fillOpacity = 1;
+
+            series.columns.template.adapter.add("fill", function(fill, target) {
+                return chart.colors.getIndex(target.dataItem.index);
             });
 
-            series.columns.template.adapters.add("stroke", (stroke, target) => {
-                return chart.get("colors").getIndex(series.columns.indexOf(target));
-            });
+// Cursor
+            chart.cursor = new am4charts.XYCursor();
 
-
-// Set data
-            let url = 'chartPedidos';
-            fetch(url)
-                .then(response => response.json() )
-                .then( datos => mostrar(datos))
-                .catch( e => console.log(e))
-
-            const mostrar = (chartPedidos)=> {
-                chart.data = chartPedidos
-                console.log(chart.data)
-            }
-            
-            xAxis.data.setAll(data);
-            series.data.setAll(data);
-
-
-// Make stuff animate on load
-// https://www.amcharts.com/docs/v5/concepts/animations/
-            series.appear(1000);
-            chart.appear(1000, 100);
-
-        }); // end am5.ready()
+        }); // end am4core.ready()
     </script>
 
 </head>
