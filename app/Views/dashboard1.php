9f985928667302f6537a0cd32ae8a59042f501ea
dowwo <roggers.morales@gmail.com>
2021-12-30 02:55:11 -0300

    Update dashboard1.php

--- app/Views/dashboard1.php
+++ app/Views/dashboard1.php
@@ -246,43 +246,16 @@
             chart.scrollbarX = new am4core.Scrollbar();
 
 // Add data
-            chart.data = [{
-                "country": "USA",
-                "visits": 3025
-            }, {
-                "country": "China",
-                "visits": 1882
-            }, {
-                "country": "Japan",
-                "visits": 1809
-            }, {
-                "country": "Germany",
-                "visits": 1322
-            }, {
-                "country": "UK",
-                "visits": 1122
-            }, {
-                "country": "France",
-                "visits": 1114
-            }, {
-                "country": "India",
-                "visits": 984
-            }, {
-                "country": "Spain",
-                "visits": 711
-            }, {
-                "country": "Netherlands",
-                "visits": 665
-            }, {
-                "country": "Russia",
-                "visits": 580
-            }, {
-                "country": "South Korea",
-                "visits": 443
-            }, {
-                "country": "Canada",
-                "visits": 441
-            }];
+            let url = 'chartPedidos';
+            fetch(url)
+                .then(response => response.json() )
+                .then( datos => mostrar(datos))
+                .catch( e => console.log(e))
+
+            const mostrar = (pedidos)=> {
+                chart.data = pedidos
+                console.log(chart.data)
+            }
 
 // Create axes
             var categoryAxis = chart.xAxes.push(new am4charts.CategoryAxis());
