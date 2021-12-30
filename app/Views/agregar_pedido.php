3c7914d51c76110eb7943cffc3dbea0538fd59c8
dowwo <roggers.morales@gmail.com>
2021-12-30 13:30:53 -0300

    Update agregar_pedido.php

--- app/Views/agregar_pedido.php
+++ app/Views/agregar_pedido.php
@@ -271,8 +271,8 @@
         }
     </style>
 
-    <script type="text/javascript"
-            src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/gmaps.js/0.4.25/gmaps.js"></script>
     <script>
         $(document).ready(function () {
