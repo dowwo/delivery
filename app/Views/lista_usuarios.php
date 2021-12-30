d2f535d2be7814793a535d9ab7c2b8102d9a6fff
dowwo <roggers.morales@gmail.com>
2021-12-30 13:49:44 -0300

    Update lista_usuarios.php

--- app/Views/lista_usuarios.php
+++ app/Views/lista_usuarios.php
@@ -45,6 +45,37 @@
         } );
     </script>
 
+    <!--Este script es para traducir el Datatable -->
+
+    <script>
+        /* Custom filtering function which will search data in column four between two values */
+        $.fn.dataTable.ext.search.push(
+            function( settings, data, dataIndex ) {
+                var min = parseInt( $('#min').val(), 10 );
+                var max = parseInt( $('#max').val(), 10 );
+                var age = parseFloat( data[3] ) || 0; // use data for the age column
+
+                if ( ( isNaN( min ) && isNaN( max ) ) ||
+                    ( isNaN( min ) && age <= max ) ||
+                    ( min <= age   && isNaN( max ) ) ||
+                    ( min <= age   && age <= max ) )
+                {
+                    return true;
+                }
+                return false;
+            }
+        );
+
+        $(document).ready(function() {
+            var table = $('#lista-usuarios').DataTable();
+
+            // Event listener to the two range filtering inputs to redraw on input
+            $('#min, #max').keyup( function() {
+                table.draw();
+            } );
+        } );
+    </script>
+
     <style>
         body {
             background-color: #AB3E5B;
