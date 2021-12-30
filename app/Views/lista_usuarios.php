1cb59c4bd5bcfe8f85e390436d2cd4a16f7ba468
dowwo <roggers.morales@gmail.com>
2021-12-30 13:48:29 -0300

    Update lista_usuarios.php

--- app/Views/lista_usuarios.php
+++ app/Views/lista_usuarios.php
@@ -22,7 +22,7 @@
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
     <script type="text/javascript" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
-<?php include 'style_listas.php'; ?>
+
     <script>
         $(document).ready( function () {
             $('#users-list').DataTable( {
