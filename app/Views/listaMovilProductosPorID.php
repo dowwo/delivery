745617a97a8d1278e7ecc333488096eb89d4f1b3
Cristofer-Sepulveda <cristofer.sepulveda02@gmail.com>
2021-12-31 04:52:14 -0300

    Update listaMovilProductosPorID.php

--- app/Views/listaMovilProductosPorID.php
+++ app/Views/listaMovilProductosPorID.php
@@ -7,15 +7,13 @@ $usuario_id_usuario=$_POST['usuario_id_usuario'];
 
 //$usuario_id_usuario=18;
 
-//$sentencia=$conexion->prepare("SELECT * FROM pedido WHERE usuario_id_usuario=?");
-//$sentencia->bind_param('s', $usuario_id_usuario);
-//$sentencia->execute();
+$sentencia=$conexion->prepare("SELECT * FROM pedido WHERE usuario_id_usuario=?");
+$sentencia->bind_param('s', $usuario_id_usuario);
+$sentencia->execute();
 
-$sql = "SELECT * FROM pedido WHERE usuario_id_usuario=".$usuario_id_usuario."";
-$result = mysqli_query($conexion, $sql) or die("Error en seleccionar ".mysqli_error($conexion));
-
-//$resultado1 = $sentencia->get_result();
-    while ($row = mysqli_fetch_array($result)){
+$resultado1 = $sentencia->get_result();
+if ($resultado1 = $sentencia->get_result()){
+    while ($row = $resultado1->fetch_array(MYSQLI_NUM)){
         $e = array();
         $e['id_pedido'] = $row[0];
         $e['usuario_id_usuario'] = $row[1];
@@ -32,10 +30,9 @@ $result = mysqli_query($conexion, $sql) or die("Error en seleccionar ".mysqli_er
         array_push($array,$e);
     }
     echo json_encode($array);
-
-    mysqli_close($conexion);
-//$sentencia->close();
-//$conexion->close();
+}
+$sentencia->close();
+$conexion->close();
 
 ?>
 
