1e85b5c12d8d96de65f710fc730ec256f7b4b10b
Cristofer-Sepulveda <cristofer.sepulveda02@gmail.com>
2021-12-31 05:19:20 -0300

    Update updatePedidoMovil.php

--- app/Views/updatePedidoMovil.php
+++ app/Views/updatePedidoMovil.php
@@ -3,9 +3,10 @@
 include 'conexion.php';
 
 $id_pedido=$_POST['id_pedido'];
+$id_estado=$_POST['estado_id_estado'];
 
-$sentencia=$conexion->prepare("UPDATE `pedido` SET `estado_id_estado` = '3' WHERE `pedido`.`id_pedido` = ?");
-$sentencia->bind_param('s', $id_pedido);
+$sentencia=$conexion->prepare("UPDATE `pedido` SET `estado_id_estado` = ? WHERE `pedido`.`id_pedido` = ?");
+$sentencia->bind_param('ss', $id_estado,$id_pedido);
 $sentencia->execute();
 $sentencia=$conexion->prepare("SELECT * FROM pedido WHERE id_pedido = ?");
 $sentencia->bind_param('s', $id_pedido);
