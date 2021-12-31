f58233dc43411ceb221ef8d32ba203e398262de9
Cristofer-Sepulveda <cristofer.sepulveda02@gmail.com>
2021-12-31 02:47:52 -0300

    Update updatePedidoMovil.php

--- app/Views/updatePedidoMovil.php
+++ app/Views/updatePedidoMovil.php
@@ -7,6 +7,9 @@ $id_pedido=$_POST['id_pedido'];
 $sentencia=$conexion->prepare("UPDATE `pedido` SET `estado_id_estado` = '3' WHERE `pedido`.`id_pedido` = ?");
 $sentencia->bind_param('s', $id_pedido);
 $sentencia->execute();
+$sentencia=$conexion->prepare("SELECT * FROM pedido WHERE id_pedido = ?");
+$sentencia->bind_param('s', $id_pedido);
+$sentencia->execute();
 
 $resultado=$sentencia->get_result();
 if ($fila = $resultado->fetch_assoc()){
