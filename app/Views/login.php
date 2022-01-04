3f99da1e716b7f704ffd33062ee22bfe3dcc4214
dowwo <roggers.morales@gmail.com>
2022-01-04 03:25:55 -0300

    Update login.php

--- app/Views/login.php
+++ app/Views/login.php
@@ -97,8 +97,6 @@
         </div>
     </div>
 <!-- Popper.js first, then Bootstrap JS -->
-    <!-- Start of HubSpot Embed Code -->
-    <script type="text/javascript" id="hs-script-loader" async defer src="//js-eu1.hs-scripts.com/25266980.js"></script>
-    <!-- End of HubSpot Embed Code -->
+    
 </body>
 </html>
